4b69a7e216d1ead3892829bbb4d3008dfc3c9f53
ari07-ari <arianacanada01@gmail.com>
2023-09-23 14:32:11 -0400

    Updated api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const users = require('./users');
 const daycares = require('./daycares');
+const kids = require('./kids');
+const bookings = require('./bookings');
 
 router.use('/users', users);
 router.use('/daycares', daycares);
+router.use('/kids', kids);
+router.use('/bookings', bookings);
 
 module.exports = router;

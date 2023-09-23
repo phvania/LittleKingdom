4391f2ff95e811f2bd237ed7bb1e30c6c56ecd49
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-23 16:07:52 -0400

    FIXED get route for a single kid

--- controllers/api/kids.js
+++ controllers/api/kids.js
@@ -3,61 +3,61 @@ const { Kids } = require('../../models');
 
 // Get all kids
 router.get('/', async (req, res) => {
-    try {
-      const kidsData = await Kids.findAll({
-        //   sorted by name
-        // order: [['name', 'ASC']],
-      });
-  
-      // UNCOMMENT NEXT to send
-      // Serialize user data so templates can read it
-      // const cleanedkids = kidsData.map((kid) => kid.get({ plain: true }));
-      // and then send serialized data into Handlebars.js
-      // res.render('kid', { kids });
+  try {
+    const kidsData = await Kids.findAll({
+      //   sorted by name
+      // order: [['name', 'ASC']],
+    });
 
-      res.status(200).json(kidsData);
+    // UNCOMMENT NEXT to send
+    // Serialize user data so templates can read it
+    // const cleanedkids = kidsData.map((kid) => kid.get({ plain: true }));
+    // and then send serialized data into Handlebars.js
+    // res.render('kid', { kids });
 
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  // get a specific kid by id
+    res.status(200).json(kidsData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// get a specific kid by id
 router.get('/:id', async (req, res) => {
-    try {
-      const getKid = await Kids.findByPk(req.params.id);
-  
-      if (!getKid) {
-        res.status(404).json({ message: 'No kid found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(getKid);
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const getKid = await Kids.findByPk(req.params.id);
+
+    if (!getKid) {
+      res.status(404).json({ message: 'No kid found with this id!' });
+      return;
     }
-  });
-  // add a kid using info in req.body
+
+    res.status(200).json(getKid);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// add a kid using info in req.body
 router.post('/', async (req, res) => {
-    /* req.body should look like this...
-    {
-      kid_firstname: "Basketball",
-      kid_lastname: ""
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+  /* req.body should look like this...
+  {
+    kid_firstname: "Basketball",
+    kid_lastname: ""
+    price: 200.00,
+    stock: 3,
+    tagIds: [1, 2, 3, 4]
+  }
+*/
   try {
     const addedKid = await Kids.create({
       ...req.body,
-    //   user_id: req.session.user_id,
+      //   user_id: req.session.user_id,
     });
 
     res.status(200).json(addedKid);
   } catch (err) {
     res.status(400).json(err);
   }
-// res.json({"message":"you hit post api/kids/"});
+  // res.json({"message":"you hit post api/kids/"});
 });
 
 // delete a specific kid by id
@@ -79,7 +79,7 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  res.json({"message":"you hit post api/kids/"});
+  res.json({ "message": "you hit post api/kids/" });
 
 });
 

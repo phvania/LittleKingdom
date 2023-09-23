9a13de5108e615ea2d9316a7c79312ae1ee425ca
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-23 15:34:17 -0400

    ADDED to model and seeds to accomodate addition of column daycares_id in bookings

--- models/index.js
+++ models/index.js
@@ -5,21 +5,27 @@ const Bookings = require('./Bookings');
 
 Users.hasMany(Kids, {
   foreignKey: 'user_id',
-  onDelete: 'SET DEFAULT'
+  onDelete:'CASCADE'
 });
-
 Kids.belongsTo(Users);
 
 Users.hasMany(Bookings, {
   foreignKey: 'user_id',
-  onDelete: 'SET DEFAULT'
+  onDelete:'CASCADE'
 });
-
 Bookings.belongsTo(Users);
+
+Kids.hasMany(Bookings, {
+  foreignKey: 'kid_id',
+  onDelete: 'CASCADE'
+})
+Bookings.belongsTo(Kids);
 
 Daycares.hasMany(Bookings, {
   foreignKey: 'daycare_id',
   onDelete: 'SET DEFAULT'
 });
+
+Bookings.belongsTo(Daycares);
 
 module.exports = { Users, Daycares, Bookings, Kids };

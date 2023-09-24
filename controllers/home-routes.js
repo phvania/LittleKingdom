61f17e3db6f51c804c6241b1b7d01ac4c0dad40f
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-24 16:06:27 -0400

    ADDED route GET all daycares to home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,24 @@ const path = require('path');
 
 const { Users, Kids, Bookings, Daycares } = require('../models');
 
+// GET All Daycares
+router.get('/', async (req, res) => {
+  try {
+    // Get all daycares and JOIN with user data
+    const daycaresData = await Daycares.findAll();
+
+    // Serialize data so the template can read it
+    const daycares = daycaresData.map((dc) => dc.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      daycares, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // GET About US page 
 router.get('/aboutUs', async (req, res) => {
   // render about us page

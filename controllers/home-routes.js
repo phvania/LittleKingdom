9bb98db379a7d05ce2788ce9962e5c71acc8ebbe
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-24 19:10:48 -0400

    ADDED route login and profile GET to homeroutes withAuth used

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,4 +32,35 @@ router.get('*', async (req, res) => {
   // render not found page
   res.sendFile(path.join(__dirname, '../public/pages/404.html'));
 });
+// GET User info
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+// GET Profile route IF withAuth middleware returns true
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;

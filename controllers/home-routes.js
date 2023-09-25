9be4ce95134f7dfba5f162bc3c3074192d1d5056
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-24 22:00:09 -0400

    ADDED Login page - comes up but goes nowhere

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,11 +27,6 @@ router.get('/aboutUs', async (req, res) => {
   // render about us page
   res.sendFile(path.join(__dirname, '../public/pages/aboutUs.html'));
 });
-// GET 404 page 
-router.get('*', async (req, res) => {
-  // render not found page
-  res.sendFile(path.join(__dirname, '../public/pages/404.html'));
-});
 // GET User info
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
@@ -66,6 +61,10 @@ router.get('/profile', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// GET 404 page 
+router.get('*', async (req, res) => {
+  // render not found page
+  res.sendFile(path.join(__dirname, '../public/pages/404.html'));
+});
 
 module.exports = router;
